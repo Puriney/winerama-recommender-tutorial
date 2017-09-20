51978ff1a338cbf3060ea9a001a0a106c5b37c3e
Puriney <youryanyun@gmail.com>
2017-09-20 13:26:55 -0400

    :feet: 读懂 tag 0.2：业务是url。url是业务的大脑、views是业务的手脚。下一步是大脑与手脚之间偶联在一起时变量名的细节。

--- winerama/reviews/urls.py
+++ winerama/reviews/urls.py
@@ -2,6 +2,18 @@ from django.conf.urls import url
 
 from . import views
 
+'''
+二级boss，就不那么简单清点url了，而是要着手处理与reviews相关的业务了。但问题是，就在urls.py里写真正的业务实现代码么？
+
+答案是“不”， 参考Django的工程学理念MTV模式：选择哪些数据给用户，实际是views来完成。
+
+因此，具体对任务的实现，url总是负责正则匹配而已，即把url对应到相关的实际业务函数。而实际业务函数都在 views.py 里。
+
+小结一下：reviews/url.py 是 reviews业务的大脑， reviews/views.py 是 reviews业务的手脚。
+
+作为开发者，需要关注的是，这两个文件里，变量之间是如何统一从而实现交互的。
+
+'''
 urlpatterns = [
     # ex: /
     url(r'^$', views.review_list, name='review_list'),
--- winerama/winerama/urls.py
+++ winerama/winerama/urls.py
@@ -17,9 +17,19 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+
+    # 一级boss最大，二级boss次之，以此类推
+
+    # url 等价于 业务。因为用户的真实目的和需求，都是通过url，来与网站互作的。有什么url，就意味着网站能提供的业务。
+    # 这里是 一级boss 统领总账（把全部url收集起来），仅仅是对二级boss手下的业务进行清点。
+
+    # 跟 reviews 相关的 二级boss 之一： 所有与reviews相关的url都交由 reviews 这个 子app （二级boss）来统领
+    # 具体看 它的脑子：reviews/urls.py 和 手脚：reviews/views.py
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
+
+    # 跟 admin 相关的 二级boss 之一：
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace="auth")),
-    
+
 ]
